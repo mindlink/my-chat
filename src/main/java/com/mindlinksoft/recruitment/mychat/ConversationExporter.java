8ca77625ff3266ca9fe5a1315b74f896b0149758
Nic Terreri <niccolo.terreri@gmail.com>
2016-06-25 13:43:35 +0100

    refactored exporter class and added CLI input test

--- src/main/java/com/mindlinksoft/recruitment/mychat/ConversationExporter.java
+++ src/main/java/com/mindlinksoft/recruitment/mychat/ConversationExporter.java
@@ -4,6 +4,8 @@ import com.google.gson.*;
 
 import java.io.*;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,93 +15,118 @@ import java.util.List;
  */
 public class ConversationExporter {
 
-    /**
-     * The application entry point.
-     * @param args The command line arguments.
-     * @throws Exception Thrown when something bad happens.
-     */
-    public static void main(String[] args) throws Exception {
-        ConversationExporter exporter = new ConversationExporter();
-        ConversationExporterConfiguration configuration = new CommandLineArgumentParser().parseCommandLineArguments(args);
+	/**Private gson instance used to serialize Java types into JSON
+	 * appropriately*/
+	private static Gson gson = null;
 
-        exporter.exportConversation(configuration.inputFilePath, configuration.outputFilePath);
-    }
+	/**
+	 * Default Constructor*/
+	public ConversationExporter() {
+		init();
+	}
 
-    /**
-     * Exports the conversation at {@code inputFilePath} as JSON to {@code outputFilePath}.
-     * @param inputFilePath The input file path.
-     * @param outputFilePath The output file path.
-     * @throws Exception Thrown when something bad happens.
-     */
-    public void exportConversation(String inputFilePath, String outputFilePath) throws Exception {
-        Conversation conversation = this.readConversation(inputFilePath);
+	/**
+	 * Mandatory instance construction instance-invariant delegate method, 
+	 * initializes static Gson variable used to serialize Java types into JSON*/
+	private static void init() {
+		if(gson == null) {
+			GsonBuilder gsonBuilder = new GsonBuilder();
+			gsonBuilder.registerTypeAdapter(Instant.class, 
+													new InstantSerializer());
 
-        this.writeConversation(conversation, outputFilePath);
+			gson = gsonBuilder.create();
+		}
+	}
 
-        // TODO: Add more logging...
-        System.out.println("Conversation exported from '" + inputFilePath + "' to '" + outputFilePath);
-    }
+	/**
+	 * The application entry point.
+	 * @param args The command line arguments.
+	 * @throws IOException TODO do not throw from main
+	 */
+	public static void main(String[] args) throws IOException {
+		ConversationExporter exporter = new ConversationExporter();
+		ConversationExporterConfiguration configuration = new CommandLineArgumentParser().parseCommandLineArguments(args);
 
-    /**
-     * Helper method to write the given {@code conversation} as JSON to the given {@code outputFilePath}.
-     * @param conversation The conversation to write.
-     * @param outputFilePath The file path where the conversation should be written.
-     * @throws Exception Thrown when something bad happens.
-     */
-    public void writeConversation(Conversation conversation, String outputFilePath) throws Exception {
-        // TODO: Do we need both to be resources, or will buffered writer close the stream?
-        try (OutputStream os = new FileOutputStream(outputFilePath, true);
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os))) {
+		exporter.exportConversation(configuration.inputFilePath, configuration.outputFilePath);
+	}
 
-            // TODO: Maybe reuse this? Make it more testable...
-            GsonBuilder gsonBuilder = new GsonBuilder();
-            gsonBuilder.registerTypeAdapter(Instant.class, new InstantSerializer());
+	/**
+	 * Exports the conversation at {@code inputFilePath} as JSON to {@code outputFilePath}.
+	 * @param inputFilePath The input file path.
+	 * @param outputFilePath The output file path.
+	 * @throws Exception Thrown when something bad happens.
+	 */
+	public void exportConversation(String inputFilePath, String outputFilePath) throws IOException {
+		
+		Conversation conversation = this.readConversation(inputFilePath);
+		this.writeConversation(conversation, outputFilePath);
 
-            Gson g = gsonBuilder.create();
+		// TODO: Add more logging...
+		System.out.println("Conversation exported from '" + inputFilePath + "' to '" + outputFilePath);
+	}
 
-            bw.write(g.toJson(conversation));
-        } catch (FileNotFoundException e) {
-            // TODO: Maybe include more information?
-            throw new IllegalArgumentException("The file was not found.");
-        } catch (IOException e) {
-            // TODO: Should probably throw different exception to be more meaningful :/
-            throw new Exception("Something went wrong");
-        }
-    }
+	/**
+	 * Helper method to write the given {@code conversation} as JSON to the given {@code outputFilePath}.
+	 * @param conversation The conversation to write.
+	 * @param outputFilePath The file path where the conversation should be written.
+	 * @throws IOException 
+	 */
+	public void writeConversation(Conversation conversation, String outputFilePath) throws IOException {
+		BufferedWriter w = null;
+		try {
+			w = Files.newBufferedWriter(Paths.get(outputFilePath));
 
-    /**
-     * Represents a helper to read a conversation from the given {@code inputFilePath}.
-     * @param inputFilePath The path to the input file.
-     * @return The {@link Conversation} representing by the input file.
-     * @throws Exception Thrown when something bad happens.
-     */
-    public Conversation readConversation(String inputFilePath) throws Exception {
-        try(InputStream is = new FileInputStream(inputFilePath);
-            BufferedReader r = new BufferedReader(new InputStreamReader(is))) {
+			w.write(gson.toJson(conversation));
+		} catch (FileNotFoundException e) {
+			// TODO: Maybe include more information?
+			// NT: like what?
+			throw new IllegalArgumentException(e.getMessage());
+		} finally {
+			w.close();
+		}
 
-            List<Message> messages = new ArrayList<Message>();
+	}
 
-            String conversationName = r.readLine();
-            String line;
+	/**
+	 * Returns Conversation object from provided {@code inputFilePath}.
+	 * @param inputFilePath The path to the input file.
+	 * @return The {@link Conversation} representing by the input file.
+	 * @throws IOException Thrown when input file IO error occurs.
+	 */
+	public Conversation readConversation(String inputFilePath) throws IOException {
+		BufferedReader r = null;
+		try {
+			r = Files.newBufferedReader(Paths.get(inputFilePath));
 
-            while ((line = r.readLine()) != null) {
-              String[] split = line.split(" ", 3);
-              String messageContent = line.substring(line.indexOf(' ', 11) + 1);
-              messages.add(new Message(Instant.ofEpochSecond(Long.parseUnsignedLong(split[0])), split[1], messageContent));
-          }
+			List<Message> messages = new ArrayList<Message>();
 
-            return new Conversation(conversationName, messages);
-        } catch (FileNotFoundException e) {
-            throw new IllegalArgumentException("The file was not found.");
-        } catch (IOException e) {
-            throw new Exception("Something went wrong");
-        }
-    }
+			String conversationName = r.readLine();
+			String line;
 
-    class InstantSerializer implements JsonSerializer<Instant> {
-        @Override
-        public JsonElement serialize(Instant instant, Type type, JsonSerializationContext jsonSerializationContext) {
-            return new JsonPrimitive(instant.getEpochSecond());
-        }
-    }
+			while ((line = r.readLine()) != null) {
+				String[] split = line.split(" ", 3);
+				String messageContent = line.substring(line.indexOf(' ', 11) + 1);
+				messages.add(new Message(
+						Instant.ofEpochSecond(Long.parseUnsignedLong(split[0])),
+						split[1], 
+						messageContent));
+			}
+
+			return new Conversation(conversationName, messages);
+		} catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("The file was not found.");
+		} finally {
+			r.close();
+		}
+	}
+
+	/**
+	 * Static inner class used to provide custom serialization into JSON for 
+	 * Instant Java class*/
+	static private class InstantSerializer implements JsonSerializer<Instant> {
+		@Override
+		public JsonElement serialize(Instant instant, Type type, JsonSerializationContext jsonSerializationContext) {
+			return new JsonPrimitive(instant.getEpochSecond());
+		}
+	}
 }
--- src/test/java/com/mindlinksoft/recruitment/mychat/ConversationExporterTests.java
+++ src/test/java/com/mindlinksoft/recruitment/mychat/ConversationExporterTests.java
@@ -1,9 +1,14 @@
 package com.mindlinksoft.recruitment.mychat;
 
 import com.google.gson.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.nio.file.FileSystems;
@@ -11,83 +16,192 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Instant;
 
-import static org.junit.Assert.assertEquals;
-
 /**
  * Tests for the {@link ConversationExporter}.
  */
 public class ConversationExporterTests {
-    /**
-     * Tests that exporting a conversation will export the conversation correctly.
-     * @throws Exception When something bad happens.
-     */
-    @Test
-    public void testExportingConversationExportsConversation() throws Exception {
-    	
-    
-        ConversationExporter exporter = new ConversationExporter();
 
-        exporter.exportConversation("chat.txt", "chat.json");
+	/**Class field*/
+	ConversationExporter exporter;
 
-        GsonBuilder builder = new GsonBuilder();
-        builder.registerTypeAdapter(Instant.class, new InstantDeserializer());
+	@Before
+	/**Perfom setup before running these class tests*/
+	public void setUp() {
+		exporter = new ConversationExporter();
+	}
 
-        Gson g = builder.create();
+	@Test
+	/**Test object returned by reading file procedure*/
+	public void testReadConversation() throws IOException {
+		Conversation c = exporter.readConversation("chat.txt");
 
-        Conversation c = g.fromJson(new InputStreamReader(new FileInputStream("chat.json")), Conversation.class);
-        Files.delete(FileSystems.getDefault().getPath("chat.json"));
-        
-        assertEquals("My Conversation", c.name);
+		assertEquals("My Conversation", c.name);
 
-        assertEquals(7, c.messages.size());
+		assertEquals(7, c.messages.size());
 
-        Message[] ms = new Message[c.messages.size()];
-        c.messages.toArray(ms);
+		Message[] ms = new Message[c.messages.size()];
+		c.messages.toArray(ms);
 
-        assertEquals(Instant.ofEpochSecond(1448470901), ms[0].timestamp);
-        assertEquals("bob",ms[0].senderId);
-        assertEquals("Hello there!", ms[0].content);
+		assertEquals(Instant.ofEpochSecond(1448470901), ms[0].timestamp);
+		assertEquals("bob",ms[0].senderId);
+		assertEquals("Hello there!", ms[0].content);
 
-        assertEquals(Instant.ofEpochSecond(1448470905), ms[1].timestamp);
-        assertEquals("mike", ms[1].senderId);
-        assertEquals("how are you?", ms[1].content);
+		assertEquals(Instant.ofEpochSecond(1448470905), ms[1].timestamp);
+		assertEquals("mike", ms[1].senderId);
+		assertEquals("how are you?", ms[1].content);
 
-        assertEquals(Instant.ofEpochSecond(1448470906), ms[2].timestamp);
-        assertEquals("bob", ms[2].senderId);
-        assertEquals("I'm good thanks, do you like pie?", ms[2].content);
+		assertEquals(Instant.ofEpochSecond(1448470906), ms[2].timestamp);
+		assertEquals("bob", ms[2].senderId);
+		assertEquals("I'm good thanks, do you like pie?", ms[2].content);
 
-        assertEquals(Instant.ofEpochSecond(1448470910), ms[3].timestamp);
-        assertEquals("mike", ms[3].senderId);
-        assertEquals("no, let me ask Angus...", ms[3].content);
+		assertEquals(Instant.ofEpochSecond(1448470910), ms[3].timestamp);
+		assertEquals("mike", ms[3].senderId);
+		assertEquals("no, let me ask Angus...", ms[3].content);
 
-        assertEquals(Instant.ofEpochSecond(1448470912), ms[4].timestamp);
-        assertEquals("angus", ms[4].senderId);
-        assertEquals("Hell yes! Are we buying some pie?", ms[4].content);
+		assertEquals(Instant.ofEpochSecond(1448470912), ms[4].timestamp);
+		assertEquals("angus", ms[4].senderId);
+		assertEquals("Hell yes! Are we buying some pie?", ms[4].content);
 
-        assertEquals(Instant.ofEpochSecond(1448470914), ms[5].timestamp);
-        assertEquals("bob", ms[5].senderId);
-        assertEquals("No, just want to know if there's anybody else in the pie society...", ms[5].content);
+		assertEquals(Instant.ofEpochSecond(1448470914), ms[5].timestamp);
+		assertEquals("bob", ms[5].senderId);
+		assertEquals("No, just want to know if there's anybody else in the pie society...", ms[5].content);
 
-        assertEquals(Instant.ofEpochSecond(1448470915), ms[6].timestamp);
-        assertEquals("angus", ms[6].senderId);
-        assertEquals("YES! I'm the head pie eater there...", ms[6].content);
-    }
+		assertEquals(Instant.ofEpochSecond(1448470915), ms[6].timestamp);
+		assertEquals("angus", ms[6].senderId);
+		assertEquals("YES! I'm the head pie eater there...", ms[6].content);
+	}
 
-    class InstantDeserializer implements JsonDeserializer<Instant> {
+	/**
+	 * @throws IOException */
+	//	@Test
+	//	public void testWriteConversation() throws IOException {
+	//		Conversation c = exporter.readConversation("chat.txt");
+	//		exporter.writeConversation(c, "chat.json");
+	//				
+	//	}
 
-        @Override
-        public Instant deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-            if (!jsonElement.isJsonPrimitive()) {
-                throw new JsonParseException("Expected instant represented as JSON number, but no primitive found.");
-            }
+	/**
+	 * Tests that exporting a conversation will export the conversation correctly.
+	 */
+	@Test
+	public void testExportConversation() throws IOException {
+		exporter = new ConversationExporter();
 
-            JsonPrimitive jsonPrimitive = jsonElement.getAsJsonPrimitive();
+		exporter.exportConversation("chat.txt", "chat.json");
 
-            if (!jsonPrimitive.isNumber()) {
-                throw new JsonParseException("Expected instant represented as JSON number, but different primitive found.");
-            }
+		GsonBuilder builder = new GsonBuilder();
+		builder.registerTypeAdapter(Instant.class, new InstantDeserializer());
 
-            return Instant.ofEpochSecond(jsonPrimitive.getAsLong());
-        }
-    }
+		Gson g = builder.create();
+
+		Conversation c = g.fromJson(new InputStreamReader(new FileInputStream("chat.json")), Conversation.class);
+		Files.delete(FileSystems.getDefault().getPath("chat.json"));
+
+		assertNotNull(c);
+		assertEquals("My Conversation", c.name);
+
+		assertEquals(7, c.messages.size());
+
+		Message[] ms = new Message[c.messages.size()];
+		c.messages.toArray(ms);
+
+		assertEquals(Instant.ofEpochSecond(1448470901), ms[0].timestamp);
+		assertEquals("bob",ms[0].senderId);
+		assertEquals("Hello there!", ms[0].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470905), ms[1].timestamp);
+		assertEquals("mike", ms[1].senderId);
+		assertEquals("how are you?", ms[1].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470906), ms[2].timestamp);
+		assertEquals("bob", ms[2].senderId);
+		assertEquals("I'm good thanks, do you like pie?", ms[2].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470910), ms[3].timestamp);
+		assertEquals("mike", ms[3].senderId);
+		assertEquals("no, let me ask Angus...", ms[3].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470912), ms[4].timestamp);
+		assertEquals("angus", ms[4].senderId);
+		assertEquals("Hell yes! Are we buying some pie?", ms[4].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470914), ms[5].timestamp);
+		assertEquals("bob", ms[5].senderId);
+		assertEquals("No, just want to know if there's anybody else in the pie society...", ms[5].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470915), ms[6].timestamp);
+		assertEquals("angus", ms[6].senderId);
+		assertEquals("YES! I'm the head pie eater there...", ms[6].content);
+	}
+
+	/**command line argument test*/
+	@Test
+	public void testCLI() throws IOException {
+		ConversationExporter.main(new String[] { "chat.txt", "chat.json" });
+
+		GsonBuilder builder = new GsonBuilder();
+		builder.registerTypeAdapter(Instant.class, new InstantDeserializer());
+
+		Gson g = builder.create();
+
+		Conversation c = g.fromJson(new InputStreamReader(new FileInputStream("chat.json")), Conversation.class);
+		Files.delete(FileSystems.getDefault().getPath("chat.json"));
+		assertNotNull(c);
+		assertEquals("My Conversation", c.name);
+
+		assertEquals(7, c.messages.size());
+
+		Message[] ms = new Message[c.messages.size()];
+		c.messages.toArray(ms);
+
+		assertEquals(Instant.ofEpochSecond(1448470901), ms[0].timestamp);
+		assertEquals("bob",ms[0].senderId);
+		assertEquals("Hello there!", ms[0].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470905), ms[1].timestamp);
+		assertEquals("mike", ms[1].senderId);
+		assertEquals("how are you?", ms[1].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470906), ms[2].timestamp);
+		assertEquals("bob", ms[2].senderId);
+		assertEquals("I'm good thanks, do you like pie?", ms[2].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470910), ms[3].timestamp);
+		assertEquals("mike", ms[3].senderId);
+		assertEquals("no, let me ask Angus...", ms[3].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470912), ms[4].timestamp);
+		assertEquals("angus", ms[4].senderId);
+		assertEquals("Hell yes! Are we buying some pie?", ms[4].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470914), ms[5].timestamp);
+		assertEquals("bob", ms[5].senderId);
+		assertEquals("No, just want to know if there's anybody else in the pie society...", ms[5].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470915), ms[6].timestamp);
+		assertEquals("angus", ms[6].senderId);
+		assertEquals("YES! I'm the head pie eater there...", ms[6].content);
+	}
+
+	private void verifyPieConversation(Conversation c) {
+
+	}
+
+	class InstantDeserializer implements JsonDeserializer<Instant> {
+
+		@Override
+		public Instant deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+			if (!jsonElement.isJsonPrimitive()) {
+				throw new JsonParseException("Expected instant represented as JSON number, but no primitive found.");
+			}
+
+			JsonPrimitive jsonPrimitive = jsonElement.getAsJsonPrimitive();
+
+			if (!jsonPrimitive.isNumber()) {
+				throw new JsonParseException("Expected instant represented as JSON number, but different primitive found.");
+			}
+
+			return Instant.ofEpochSecond(jsonPrimitive.getAsLong());
+		}
+	}
 }
