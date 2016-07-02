65cc81475bb71753dfa03c93fc2b3f7b4ebecee7
Nic Terreri <niccolo.terreri@gmail.com>
2016-07-02 12:45:26 +0100

    implemented filtering by word occurrence

--- src/main/java/com/mindlinksoft/recruitment/mychat/FilterKeyword.java
+++ src/main/java/com/mindlinksoft/recruitment/mychat/FilterKeyword.java
@@ -1,16 +1,29 @@
 package com.mindlinksoft.recruitment.mychat;
 
+import java.util.Iterator;
+
 public class FilterKeyword implements ConversationFilter {
-	
+
 	private String keyword;
-	
+
 	FilterKeyword(String keyword) {
-		
+		this.keyword = keyword;
+
 	}
 
 	@Override
 	public void apply(Conversation conversation) {
-		// TODO Auto-generated method stub
+		
+		Iterator<Message> itr = conversation.messages.iterator(); 
+		
+		while(itr.hasNext()) {
+			Message next = itr.next();
+			
+			//remove message if substring does not match content
+			if(!next.content.toLowerCase().matches("(.*)" + 
+					keyword.toLowerCase() + "(.*)")) 
+				itr.remove();
+		}
 
 	}
 
---
+++ src/test/java/com/mindlinksoft/recruitment/mychat/FilterKeywordTests.java
@@ -0,0 +1,57 @@
+package com.mindlinksoft.recruitment.mychat;
+
+import static org.junit.Assert.*;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.time.Instant;
+
+import org.junit.Test;
+
+public class FilterKeywordTests {
+
+	@Test
+	public void testConstructor() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		FilterKeyword f = new FilterKeyword("word");
+		
+		Field keywordField = FilterKeyword.class.getDeclaredField("keyword");
+		keywordField.setAccessible(true);
+		String keywordValue = (String) keywordField.get(f);
+		
+		assertTrue(keywordValue.compareTo("word") == 0);
+		assertFalse(keywordValue.compareTo("nonsense") == 0);
+	}
+	
+	@Test
+	public void testApply() throws IOException {
+		ConversationReader reader = new ConversationReader(new FileReader("chat.txt"));
+		Conversation conversation = reader.readConversation();
+		reader.close();
+		
+		new FilterKeyword("pie").apply(conversation);
+		
+		assertEquals(4, conversation.messages.size());
+		Message[] ms = new Message[conversation.messages.size()];
+		conversation.messages.toArray(ms);
+		
+		assertEquals(Instant.ofEpochSecond(1448470906), ms[0].timestamp);
+		assertEquals("bob", ms[0].senderId);
+		assertEquals("I'm good thanks, do you like pie?", ms[0].content);
+		
+		assertEquals(Instant.ofEpochSecond(1448470912), ms[1].timestamp);
+		assertEquals("angus", ms[1].senderId);
+		assertEquals("Hell yes! Are we buying some pie?", ms[1].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470914), ms[2].timestamp);
+		assertEquals("bob", ms[2].senderId);
+		assertEquals("No, just want to know if there's anybody else in the pie society...", ms[2].content);
+
+		assertEquals(Instant.ofEpochSecond(1448470915), ms[3].timestamp);
+		assertEquals("angus", ms[3].senderId);
+		assertEquals("YES! I'm the head pie eater there...", ms[3].content);
+		
+
+	}
+
+}
