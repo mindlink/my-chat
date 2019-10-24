07c27a1432db7ca8e59f424c542acbd252b3857f
MariaelenaaYianni <56835722+MariaelenaaYianni@users.noreply.github.com>
2019-10-24 23:46:36 +0300

    Update Conversation.java

--- src/main/java/com/mindlinksoft/recruitment/mychat/Conversation.java
+++ src/main/java/com/mindlinksoft/recruitment/mychat/Conversation.java
@@ -1,6 +1,7 @@
 package com.mindlinksoft.recruitment.mychat;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Represents the model of a conversation.
@@ -15,6 +16,8 @@ public final class Conversation {
      * The messages in the conversation.
      */
     public Collection<Message> messages;
+    
+    public List<UserReport> report;
 
     /**
      * Initializes a new instance of the {@link Conversation} class.
