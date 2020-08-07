31a892bd3be3be1e9edc4bee22e747a8c4a124e5
Jonis Abdulkarim <jonis.abdulkarim@protonmail.com>
2020-08-07 19:09:20 +0100

    Code and test Sender

--- src/main/java/com/mindlinksoft/recruitment/mychat/Sender.java
+++ src/main/java/com/mindlinksoft/recruitment/mychat/Sender.java
@@ -1,5 +1,45 @@
 package com.mindlinksoft.recruitment.mychat;
 
 public class Sender {
-    
+
+    private static transient long senderCount; // used to set id
+    private transient long senderId; // for equals() comparison
+    private String senderText; 
+
+    Sender(String senderText) {
+        this.senderId = senderCount++;
+        this.senderText = senderText;
+    }
+
+    /**
+     * Returns this sender's unique identifier
+     * 
+     * @return unique identifier in long
+     */
+    public long getSenderId() {
+        return this.senderId;
+    }
+
+    /**
+     * Returns the sender's name, as it appears in text
+     * 
+     * @return sender's name, as it appears in text
+     */
+    public String getSenderText() {
+        return senderText;
+    }
+
+    /**
+     * Checks if given object is same sender as this
+     * 
+     * @return true if senderId is equal, else false
+     */
+    public boolean equals(Object other) {
+        if (other instanceof Sender) {
+            Sender otherSender = (Sender) other;
+            return otherSender.getSenderId() == this.getSenderId();
+        } else {
+            return false;
+        }
+    }
 }
--- src/test/java/com/mindlinksoft/recruitment/mychat/SenderTests.java
+++ src/test/java/com/mindlinksoft/recruitment/mychat/SenderTests.java
@@ -1,5 +1,48 @@
 package com.mindlinksoft.recruitment.mychat;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class SenderTests {
     
+    Sender senderTom;
+    Sender senderJames;
+    Sender senderRick;
+
+    @Before
+    public void setUp() {
+        senderTom = new Sender("Tom");
+        senderJames = new Sender("James");
+        senderRick = new Sender("Rick");
+    }
+
+    @Test
+    public void senderHaveCorrectText() {
+        assertEquals("Tom", senderTom.getSenderText());
+        assertEquals("James", senderJames.getSenderText());
+        assertEquals("Rick", senderRick.getSenderText());
+    }
+
+    @Test
+    public void haveUniqueSenderId() {
+        // senders have unique ids
+        assertNotEquals(senderTom.getSenderId(), senderJames.getSenderId());
+        assertNotEquals(senderRick.getSenderId(), senderJames.getSenderId());
+    }
+
+    @Test
+    public void notEqualIfNotSenderType() {
+        assertNotEquals(null, senderTom);
+        assertNotEquals("hello", senderJames);
+    }
+
+    @Test
+    public void equalIfSameId() {
+        assertEquals(senderTom, senderTom);
+        assertEquals(senderRick, senderRick);
+        assertEquals(senderJames, senderJames);
+    }
 }
