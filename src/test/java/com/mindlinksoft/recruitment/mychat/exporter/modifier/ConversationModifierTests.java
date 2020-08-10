33183b5b303e523bb79477c4a8dec2510604d1ed
Jonis Abdulkarim <jonis.abdulkarim@protonmail.com>
2020-08-10 20:00:59 +0100

    Test ConversationModifier

--- src/test/java/com/mindlinksoft/recruitment/mychat/exporter/modifier/ConversationModifierTests.java
+++ src/test/java/com/mindlinksoft/recruitment/mychat/exporter/modifier/ConversationModifierTests.java
@@ -1,11 +1,38 @@
 package com.mindlinksoft.recruitment.mychat.exporter.modifier;
 
+import static org.junit.Assert.assertTrue;
+
+import com.mindlinksoft.recruitment.mychat.exporter.datastructure.Conversation;
+import com.mindlinksoft.recruitment.mychat.exporter.modifier.filter.FilterKeyWord;
+import com.mindlinksoft.recruitment.mychat.exporter.modifier.filter.FilterUser;
+import com.mindlinksoft.recruitment.mychat.exporter.modifier.hide.HideKeyWord;
+
 import org.junit.Test;
 
 public class ConversationModifierTests {
     
+    ConversationModifier conversationModifier;
+
+    Conversation conversation;
+
     @Test
-    public void hideUser() {
-        
+    public void setUp() {
+        conversation = new Conversation(); // empty conversation
+    }
+
+    @Test
+    public void chooseModifier() {
+        // filter key word should return relevant class
+        conversationModifier = new ConversationModifier(conversation, Modifier.FILTER_KEYWORD, "pie");
+        ModifierBase result = conversationModifier.chooseModification();
+        assertTrue(result instanceof FilterKeyWord);
+
+        conversationModifier = new ConversationModifier(conversation, Modifier.FILTER_USER, "bob");
+        result = conversationModifier.chooseModification();
+        assertTrue(result instanceof FilterUser);
+
+        conversationModifier = new ConversationModifier(conversation, Modifier.HIDE_KEYWORD, "pie");
+        result = conversationModifier.chooseModification();
+        assertTrue(result instanceof HideKeyWord);
     }
 }
