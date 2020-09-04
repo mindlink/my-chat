f380027edddcafeceaee248212f5635102e09e1a
alexsikorski <alexsikorski22@gmail.com>
2020-09-04 14:35:51 +0100

    Created separate class for initialising gson.

---
+++ src/main/java/com/mindlinksoft/recruitment/mychat/BuildCreateGson.java
@@ -0,0 +1,23 @@
+package com.mindlinksoft.recruitment.mychat;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.time.Instant;
+
+class BuildCreateGson {
+    Gson g;
+
+    BuildCreateGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(Instant.class, new BuildCreateGson.InstantSerializer());
+        g = gsonBuilder.create();
+    }
+
+    class InstantSerializer implements JsonSerializer<Instant> {
+        @Override
+        public JsonElement serialize(Instant instant, Type type, JsonSerializationContext jsonSerializationContext) {
+            return new JsonPrimitive(instant.getEpochSecond());
+        }
+    }
+}
