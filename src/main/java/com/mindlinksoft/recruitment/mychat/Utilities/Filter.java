24c83f0f4426f2e6a3f70240f4715ab1c93dd49b
alexsikorski <alexsikorski22@gmail.com>
2020-09-05 14:22:59 +0100

    edited chat.txt to demonstrate regex expression functionality, expression now covers generic credit card number formats

--- src/main/java/com/mindlinksoft/recruitment/mychat/Utilities/Filter.java
+++ src/main/java/com/mindlinksoft/recruitment/mychat/Utilities/Filter.java
@@ -55,7 +55,9 @@ public class Filter {
         Conversation filteredCon = new Conversation(conversation.name, messages);
 
         for (Message message : conversation.messages){
-            message.content = message.content.replaceAll("(\\(?\\+44\\)?\\s?([12378])\\d{3}|\\(?(01|02|03|07|08)\\d{3}\\)?)\\s?\\d{3}\\s?\\d{3}|(\\(?\\+44\\)?\\s?([123578])\\d{2}|\\(?(01|02|03|05|07|08)\\d{2}\\)?)\\s?\\d{3}\\s?\\d{4}|(\\(?\\+44\\)?\\s?([59])\\d{2}|\\(?(05|09)\\d{2}\\)?)\\s?\\d{3}\\s?\\d{3}", "*redacted*");
+            String messageContent = message.content.replaceAll("(\\(?\\+44\\)?\\s?([12378])\\d{3}|\\(?(01|02|03|07|08)\\d{3}\\)?)\\s?\\d{3}\\s?\\d{3}|(\\(?\\+44\\)?\\s?([123578])\\d{2}|\\(?(01|02|03|05|07|08)\\d{2}\\)?)\\s?\\d{3}\\s?\\d{4}|(\\(?\\+44\\)?\\s?([59])\\d{2}|\\(?(05|09)\\d{2}\\)?)\\s?\\d{3}\\s?\\d{3}", "*redacted*");
+            messageContent = messageContent.replaceAll("\\b((\\d{4})-? ?(\\d{4})-? ?(\\d{4})-? ?(\\d{4}))\\b","*redacted*");
+            message.content = messageContent;
             filteredCon.messages.add(message);
         }
 
