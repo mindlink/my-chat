0e5b5cd700e54a524f126425096f5494e4d8703c
alexsikorski <alexsikorski22@gmail.com>
2020-09-05 17:52:02 +0100

    generateActivityData now uses a HashMap to assign each user a number representing how many messages they have sent

--- src/main/java/com/mindlinksoft/recruitment/mychat/Utilities/Report.java
+++ src/main/java/com/mindlinksoft/recruitment/mychat/Utilities/Report.java
@@ -6,24 +6,42 @@ import com.mindlinksoft.recruitment.mychat.Objects.Message;
 import com.mindlinksoft.recruitment.mychat.Objects.User;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Report {
 
     private static Conversation conversation;
+    private static HashMap<String, Integer> userActivity = new HashMap<>();
 
     public static void generateActivityData(Conversation conversation) {
         Report.conversation = conversation;
+
+        // populate unique id
+        for (Message message : conversation.messages) {
+            if (!userActivity.containsKey(message.senderId)) {
+                userActivity.put(message.senderId, 0);
+            }
+        }
+
+        for (Message message : conversation.messages) {
+            for (Object o : userActivity.entrySet()) {
+                Map.Entry obj = (Map.Entry) o;
+                if (message.senderId.contains((CharSequence) obj.getKey())){
+                    Integer currentValue = (Integer) obj.getValue();
+                    currentValue += 1;
+                    obj.setValue(currentValue);
+                }
+            }
+        }
+        System.out.println(userActivity);
     }
 
     public static ConversationExtended generateReport() {
         List<User> users = new ArrayList<>();
         ConversationExtended outputConExt = new ConversationExtended(conversation.name,conversation.messages, users);
 
-        for (Message message : outputConExt.messages)
-        {
-
-        }
 
         return outputConExt;
     }
