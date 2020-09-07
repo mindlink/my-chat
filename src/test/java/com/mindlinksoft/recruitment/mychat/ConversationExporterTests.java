2702b5e75bb3864c50a96de90826fab3db1fd508
alexsikorski <alexsikorski22@gmail.com>
2020-09-07 12:55:44 +0100

    Added test for filtering by user

--- src/test/java/com/mindlinksoft/recruitment/mychat/ConversationExporterTests.java
+++ src/test/java/com/mindlinksoft/recruitment/mychat/ConversationExporterTests.java
@@ -35,7 +35,7 @@ public class ConversationExporterTests {
 
         assertEquals("My Conversation", c.name);
 
-        assertEquals(7, c.messages.size());
+        assertEquals(9, c.messages.size());
 
         Message[] ms = new Message[c.messages.size()];
         c.messages.toArray(ms);
@@ -67,6 +67,51 @@ public class ConversationExporterTests {
         assertEquals(ms[6].timestamp, Instant.ofEpochSecond(1448470915));
         assertEquals(ms[6].senderId, "angus");
         assertEquals(ms[6].content, "YES! I'm the head pie eater there...");
+
+        assertEquals(ms[7].timestamp, Instant.ofEpochSecond(1448470916));
+        assertEquals(ms[7].senderId, "bob");
+        assertEquals(ms[7].content, "Here's my credit card number: 1234-1234-1234-1234");
+
+        assertEquals(ms[8].timestamp, Instant.ofEpochSecond(	1448470917));
+        assertEquals(ms[8].senderId, "angus");
+        assertEquals(ms[8].content, "Here's my mobile number 07441231495 and my credit card number is 1234 3256 6483 1234");
+    }
+
+    @Test
+    public void testFilterByUser() throws Exception {
+        ConversationExporter exporter = new ConversationExporter();
+
+        exporter.exportConversation("chat.txt", "chat.json", "-name", "bob","","","");
+
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(Instant.class, new InstantDeserializer());
+
+        Gson g = builder.create();
+
+        Conversation c = g.fromJson(new InputStreamReader(new FileInputStream("chat.json")), Conversation.class);
+
+        assertEquals("My Conversation", c.name);
+
+        assertEquals(4, c.messages.size());
+
+        Message[] ms = new Message[c.messages.size()];
+        c.messages.toArray(ms);
+
+        assertEquals(ms[0].timestamp, Instant.ofEpochSecond(1448470901));
+        assertEquals(ms[0].senderId, "bob");
+        assertEquals(ms[0].content, "Hello there!");
+
+        assertEquals(ms[1].timestamp, Instant.ofEpochSecond(1448470906));
+        assertEquals(ms[1].senderId, "bob");
+        assertEquals(ms[1].content, "I'm good thanks, do you like pie?");
+
+        assertEquals(ms[2].timestamp, Instant.ofEpochSecond(1448470914));
+        assertEquals(ms[2].senderId, "bob");
+        assertEquals(ms[2].content, "No, just want to know if there's anybody else in the pie society...");
+
+        assertEquals(ms[3].timestamp, Instant.ofEpochSecond(1448470916));
+        assertEquals(ms[3].senderId, "bob");
+        assertEquals(ms[3].content, "Here's my credit card number: 1234-1234-1234-1234");
     }
 
     class InstantDeserializer implements JsonDeserializer<Instant> {
@@ -86,4 +131,6 @@ public class ConversationExporterTests {
             return Instant.ofEpochSecond(jsonPrimitive.getAsLong());
         }
     }
+
+
 }
