7830c1d512acaa1da1174f09df1e7e375d3d1d49
yaraalanezi <71646592+yaraalanezi@users.noreply.github.com>
2020-09-21 22:34:23 +0100

    Create FormatCL.java

---
+++ src/main/java/com/mindlinksoft/recruitment/mychat/FormatCL.java
@@ -0,0 +1,25 @@
+package com.mindlinksoft.recruitment.mychat;
+
+public class FormalCL {
+
+   
+    static String formatExplanation() {
+        return "\nHello, now that the entire conversation has been exported "
+                + "you can have messages filtered based on users or keywords.\n"
+                + "In addition, you conceal particular keywords in a message or"
+                + "export a conversation with unknown users.\n"
+                + "(ex. command [argument])\n";
+
+    }
+
+    static String formatMainMenuPrompt() {
+        return "\nEnter command: "
+                + "user [userID], "
+                + "keyword [keyword], "
+                + "hide [keyword], "
+                + "hideUsers, "
+                + "exit"
+                + "\n> ";
+    }
+
+}
