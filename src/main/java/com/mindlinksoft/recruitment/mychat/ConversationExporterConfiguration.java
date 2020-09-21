1767d2dc1e51d995dd92756c428ce0f9abbd7bd8
yaraalanezi <71646592+yaraalanezi@users.noreply.github.com>
2020-09-21 22:51:01 +0100

    Update ConversationExporterConfiguration.java

--- src/main/java/com/mindlinksoft/recruitment/mychat/ConversationExporterConfiguration.java
+++ src/main/java/com/mindlinksoft/recruitment/mychat/ConversationExporterConfiguration.java
@@ -1,9 +1,7 @@
 package com.mindlinksoft.recruitment.mychat;
 
-/**
- * Represents the configuration for the exporter.
- */
 public final class ConversationExporterConfiguration {
+
     /**
      * Gets the input file path.
      */
@@ -14,11 +12,6 @@ public final class ConversationExporterConfiguration {
      */
     public String outputFilePath;
 
-    /**
-     * Initializes a new instance of the {@link ConversationExporterConfiguration} class.
-     * @param inputFilePath The input file path.
-     * @param outputFilePath The output file path.
-     */
     public ConversationExporterConfiguration(String inputFilePath, String outputFilePath) {
         this.inputFilePath = inputFilePath;
         this.outputFilePath = outputFilePath;
