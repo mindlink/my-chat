a78367e8c3f07d7b914ac0b61ecc9e32d5d17b19
twooliston <70709801+twooliston@users.noreply.github.com>
2021-02-26 16:31:05 +0000

    added integration tests to new files

--- src/test/java/com/mindlinksoft/recruitment/mychat/BlacklistTest.java
+++ src/test/java/com/mindlinksoft/recruitment/mychat/BlacklistTest.java
@@ -15,88 +15,137 @@ import org.junit.Test;
 
 public class BlacklistTest {
 
-    /**
-     * Test the blacklist option.
-     * 
-     * @throws FileNotFoundException Thrown when the the input is illegal
-     * @throws IOException           Thrown when the writting to the output file
-     *                               fails
-     */
-    @Test
-    public void firstTestBlacklist() throws FileNotFoundException, IOException {
-        Conversation conversation = new OptionsTests().generateFakeConversation();
-        String[] blacklist = new String[] { "Empire", "Damn" };
-        conversation.messages = new Blacklist(conversation, blacklist).process();
+        /**
+         * Integration test for the blacklist option.
+         * 
+         * @throws FileNotFoundException Thrown when the the input is illegal
+         * @throws IOException           Thrown when the writting to the output file
+         *                               fails
+         */
+        @Test
+        public void testBlacklist() throws FileNotFoundException, IOException {
+                ConversationExporter exporter = new ConversationExporter();
 
-        Collection<Message> messages = conversation.messages;
-        Message[] messageArray = new Message[messages.size()];
-        messages.toArray(messageArray);
+                // fake configuration
+                ConversationExporterConfiguration configuration = new ConversationExporterConfiguration();
+                configuration.inputFilePath = "chat.txt";
+                configuration.outputFilePath = "testChat.json";
+                configuration.blacklist = new String[] { "Empire", "Damn" };
 
-        assertEquals(4, messageArray.length);
+                // fake conversation
+                Conversation conversation = new OptionsTests().generateFakeConversation();
+                // run through the rest of the conversation exporter
+                exporter.applyOptions(conversation, configuration);
+                exporter.writeConversation(conversation, configuration.outputFilePath);
 
-        assertEquals(Instant.ofEpochSecond(1448470901), messageArray[0].getTimestamp());
-        assertEquals("Ralof", messageArray[0].senderId);
-        assertEquals(
-                "Hey, you. You’re finally awake. You were trying to cross the border, right? Walked "
-                        + "right into that Imperial ambush, same as us, and that thief over there.",
-                messageArray[0].getContent());
+                Collection<Message> messages = conversation.getMessages();
+                Message[] messageArray = new Message[messages.size()];
+                messages.toArray(messageArray);
 
-        assertEquals(Instant.ofEpochSecond(1448470902), messageArray[1].getTimestamp());
-        assertEquals("Lokir", messageArray[1].senderId);
-        assertEquals("*redacted* you Stormcloaks. Skyrim was fine until you came along. *redacted* was nice and lazy. "
-                + "If they hadn’t been looking for you, I could’ve stolen that horse and been half way "
-                + "to Hammerfell. You there. You and me — we should be here. It’s these Stormcloaks the "
-                + "*redacted* wants.", messageArray[1].getContent());
+                assertEquals(4, messageArray.length);
 
-        assertEquals(Instant.ofEpochSecond(1448470903), messageArray[2].getTimestamp());
-        assertEquals("Ralof", messageArray[2].getSenderId());
-        assertEquals("We’re all brothers and sisters in binds now, thief.", messageArray[2].getContent());
+                assertEquals(Instant.ofEpochSecond(1448470901), messageArray[0].getTimestamp());
+                assertEquals("Ralof", messageArray[0].senderId);
+                assertEquals("Hey, you. You’re finally awake. You were trying to cross the border, right? Walked "
+                                + "right into that Imperial ambush, same as us, and that thief over there.",
+                                messageArray[0].getContent());
 
-        assertEquals(Instant.ofEpochSecond(1448470904), messageArray[3].getTimestamp());
-        assertEquals("Imperial Soldier", messageArray[3].getSenderId());
-        assertEquals("Shut up back there!", messageArray[3].getContent());
-    }
+                assertEquals(Instant.ofEpochSecond(1448470902), messageArray[1].getTimestamp());
+                assertEquals("Lokir", messageArray[1].senderId);
+                assertEquals("*redacted* you Stormcloaks. Skyrim was fine until you came along. *redacted* was nice and lazy. "
+                                + "If they hadn’t been looking for you, I could’ve stolen that horse and been half way "
+                                + "to Hammerfell. You there. You and me — we should be here. It’s these Stormcloaks the "
+                                + "*redacted* wants.", messageArray[1].getContent());
 
-    /**
-     * Test the blacklist option.
-     * 
-     * @throws FileNotFoundException Thrown when the the input is illegal
-     * @throws IOException           Thrown when the writting to the output file
-     *                               fails
-     */
-    @Test
-    public void secondTestBlacklist() throws FileNotFoundException, IOException {
-        Conversation conversation = new OptionsTests().generateFakeConversation();
-        String[] blacklist = new String[] { "Imperial", "Stormcloaks" };
-        conversation.messages = new Blacklist(conversation, blacklist).process();
+                assertEquals(Instant.ofEpochSecond(1448470903), messageArray[2].getTimestamp());
+                assertEquals("Ralof", messageArray[2].getSenderId());
+                assertEquals("We’re all brothers and sisters in binds now, thief.", messageArray[2].getContent());
 
-        Collection<Message> messages = conversation.messages;
-        Message[] messageArray = new Message[messages.size()];
-        messages.toArray(messageArray);
+                assertEquals(Instant.ofEpochSecond(1448470904), messageArray[3].getTimestamp());
+                assertEquals("Imperial Soldier", messageArray[3].getSenderId());
+                assertEquals("Shut up back there!", messageArray[3].getContent());
+        }
 
-        assertEquals(4, messageArray.length);
+        /**
+         * First Unit test for the blacklist option.
+         * 
+         * @throws FileNotFoundException Thrown when the the input is illegal
+         * @throws IOException           Thrown when the writting to the output file
+         *                               fails
+         */
+        @Test
+        public void firstTestBlacklist() throws FileNotFoundException, IOException {
+                Conversation conversation = new OptionsTests().generateFakeConversation();
+                String[] blacklist = new String[] { "Empire", "Damn" };
+                conversation.messages = new Blacklist(conversation, blacklist).process();
 
-        assertEquals(Instant.ofEpochSecond(1448470901), messageArray[0].getTimestamp());
-        assertEquals("Ralof", messageArray[0].senderId);
-        assertEquals(
-                "Hey, you. You’re finally awake. You were trying to cross the border, right? Walked "
-                        + "right into that *redacted* ambush, same as us, and that thief over there.",
-                messageArray[0].getContent());
+                Collection<Message> messages = conversation.messages;
+                Message[] messageArray = new Message[messages.size()];
+                messages.toArray(messageArray);
 
-        assertEquals(Instant.ofEpochSecond(1448470902), messageArray[1].getTimestamp());
-        assertEquals("Lokir", messageArray[1].senderId);
-        assertEquals("Damn you *redacted*. Skyrim was fine until you came along. Empire was nice and lazy. "
-                + "If they hadn’t been looking for you, I could’ve stolen that horse and been half way "
-                + "to Hammerfell. You there. You and me — we should be here. It’s these *redacted* the "
-                + "Empire wants.", messageArray[1].getContent());
+                assertEquals(4, messageArray.length);
 
-        assertEquals(Instant.ofEpochSecond(1448470903), messageArray[2].getTimestamp());
-        assertEquals("Ralof", messageArray[2].getSenderId());
-        assertEquals("We’re all brothers and sisters in binds now, thief.", messageArray[2].getContent());
+                assertEquals(Instant.ofEpochSecond(1448470901), messageArray[0].getTimestamp());
+                assertEquals("Ralof", messageArray[0].senderId);
+                assertEquals("Hey, you. You’re finally awake. You were trying to cross the border, right? Walked "
+                                + "right into that Imperial ambush, same as us, and that thief over there.",
+                                messageArray[0].getContent());
 
-        assertEquals(Instant.ofEpochSecond(1448470904), messageArray[3].getTimestamp());
-        assertEquals("Imperial Soldier", messageArray[3].getSenderId());
-        assertEquals("Shut up back there!", messageArray[3].getContent());
-    }
+                assertEquals(Instant.ofEpochSecond(1448470902), messageArray[1].getTimestamp());
+                assertEquals("Lokir", messageArray[1].senderId);
+                assertEquals("*redacted* you Stormcloaks. Skyrim was fine until you came along. *redacted* was nice and lazy. "
+                                + "If they hadn’t been looking for you, I could’ve stolen that horse and been half way "
+                                + "to Hammerfell. You there. You and me — we should be here. It’s these Stormcloaks the "
+                                + "*redacted* wants.", messageArray[1].getContent());
+
+                assertEquals(Instant.ofEpochSecond(1448470903), messageArray[2].getTimestamp());
+                assertEquals("Ralof", messageArray[2].getSenderId());
+                assertEquals("We’re all brothers and sisters in binds now, thief.", messageArray[2].getContent());
+
+                assertEquals(Instant.ofEpochSecond(1448470904), messageArray[3].getTimestamp());
+                assertEquals("Imperial Soldier", messageArray[3].getSenderId());
+                assertEquals("Shut up back there!", messageArray[3].getContent());
+        }
+
+        /**
+         * Second Unit test for the blacklist option.
+         * 
+         * @throws FileNotFoundException Thrown when the the input is illegal
+         * @throws IOException           Thrown when the writting to the output file
+         *                               fails
+         */
+        @Test
+        public void secondTestBlacklist() throws FileNotFoundException, IOException {
+                Conversation conversation = new OptionsTests().generateFakeConversation();
+                String[] blacklist = new String[] { "Imperial", "Stormcloaks" };
+                conversation.messages = new Blacklist(conversation, blacklist).process();
+
+                Collection<Message> messages = conversation.messages;
+                Message[] messageArray = new Message[messages.size()];
+                messages.toArray(messageArray);
+
+                assertEquals(4, messageArray.length);
+
+                assertEquals(Instant.ofEpochSecond(1448470901), messageArray[0].getTimestamp());
+                assertEquals("Ralof", messageArray[0].senderId);
+                assertEquals("Hey, you. You’re finally awake. You were trying to cross the border, right? Walked "
+                                + "right into that *redacted* ambush, same as us, and that thief over there.",
+                                messageArray[0].getContent());
+
+                assertEquals(Instant.ofEpochSecond(1448470902), messageArray[1].getTimestamp());
+                assertEquals("Lokir", messageArray[1].senderId);
+                assertEquals("Damn you *redacted*. Skyrim was fine until you came along. Empire was nice and lazy. "
+                                + "If they hadn’t been looking for you, I could’ve stolen that horse and been half way "
+                                + "to Hammerfell. You there. You and me — we should be here. It’s these *redacted* the "
+                                + "Empire wants.", messageArray[1].getContent());
+
+                assertEquals(Instant.ofEpochSecond(1448470903), messageArray[2].getTimestamp());
+                assertEquals("Ralof", messageArray[2].getSenderId());
+                assertEquals("We’re all brothers and sisters in binds now, thief.", messageArray[2].getContent());
+
+                assertEquals(Instant.ofEpochSecond(1448470904), messageArray[3].getTimestamp());
+                assertEquals("Imperial Soldier", messageArray[3].getSenderId());
+                assertEquals("Shut up back there!", messageArray[3].getContent());
+        }
 
 }
--- src/test/java/com/mindlinksoft/recruitment/mychat/FilterByUserTest.java
+++ src/test/java/com/mindlinksoft/recruitment/mychat/FilterByUserTest.java
@@ -16,14 +16,54 @@ import org.junit.Test;
 public class FilterByUserTest {
 
         /**
-         * Test the filter by user option.
+         * Integration test for the filter by user option.
          * 
          * @throws FileNotFoundException Thrown when the the input is illegal
          * @throws IOException           Thrown when the writting to the output file
          *                               fails
          */
         @Test
-        public void firstTestFilterByUser() throws FileNotFoundException, IOException {
+        public void testFilterByUser() throws FileNotFoundException, IOException {
+                ConversationExporter exporter = new ConversationExporter();
+
+                // fake configuration
+                ConversationExporterConfiguration configuration = new ConversationExporterConfiguration();
+                configuration.inputFilePath = "chat.txt";
+                configuration.outputFilePath = "testChat.json";
+                configuration.filterUser = "Ralof";
+
+                // fake conversation
+                Conversation conversation = new OptionsTests().generateFakeConversation();
+                // run through the rest of the conversation exporter
+                exporter.applyOptions(conversation, configuration);
+                exporter.writeConversation(conversation, configuration.outputFilePath);
+
+                Collection<Message> messages = conversation.getMessages();
+                Message[] messageArray = new Message[messages.size()];
+                messages.toArray(messageArray);
+
+                assertEquals(2, messageArray.length);
+
+                assertEquals(Instant.ofEpochSecond(1448470901), messageArray[0].getTimestamp());
+                assertEquals("Ralof", messageArray[0].senderId);
+                assertEquals("Hey, you. You’re finally awake. You were trying to cross the border, right? Walked "
+                                + "right into that Imperial ambush, same as us, and that thief over there.",
+                                messageArray[0].getContent());
+
+                assertEquals(Instant.ofEpochSecond(1448470903), messageArray[1].getTimestamp());
+                assertEquals("Ralof", messageArray[1].getSenderId());
+                assertEquals("We’re all brothers and sisters in binds now, thief.", messageArray[1].getContent());
+        }
+
+        /**
+         * First Unit test for the filter by user option.
+         * 
+         * @throws FileNotFoundException Thrown when the the input is illegal
+         * @throws IOException           Thrown when the writting to the output file
+         *                               fails
+         */
+        @Test
+        public void firstUnitTestFilterByUser() throws FileNotFoundException, IOException {
                 Conversation conversation = new OptionsTests().generateFakeConversation();
                 String filterUser = "Ralof";
                 conversation.messages = new FilterByUser(conversation, filterUser).process();
@@ -46,14 +86,14 @@ public class FilterByUserTest {
         }
 
         /**
-         * Test the filter by user option.
+         * Second Unit test for the filter by user option.
          * 
          * @throws FileNotFoundException Thrown when the the input is illegal
          * @throws IOException           Thrown when the writting to the output file
          *                               fails
          */
         @Test
-        public void secondTestFilterByUser() throws FileNotFoundException, IOException {
+        public void secondUnitTestFilterByUser() throws FileNotFoundException, IOException {
                 Conversation conversation = new OptionsTests().generateFakeConversation();
                 String filterUser = "Imperial Soldier";
                 conversation.messages = new FilterByUser(conversation, filterUser).process();
