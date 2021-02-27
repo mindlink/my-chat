255e8bb35ec479a569e7eecfc93249bdfadd2fd8
Julian Kyotaro Kubelec <juliankubelec23@gmail.com>
2021-02-27 14:10:19 +0000

    Implemented blacklistWord in ConversationBuilder.Added buildRegrex to create a pattern to find correct word.  Added jUnit test to test functionality

--- src/main/java/com/mindlinksoft/recruitment/mychat/ConversationBuilder.java
+++ src/main/java/com/mindlinksoft/recruitment/mychat/ConversationBuilder.java
@@ -7,7 +7,7 @@ import java.util.List;
  * Used to manipulate the conversation according to command-line arguments
  */
 public class ConversationBuilder {
-    private Conversation conversation;
+    private final Conversation conversation;
     private final String redacted = "*redacted*";
     /**
      * Initialises instance of a ConversationConfigurator
@@ -67,7 +67,37 @@ public class ConversationBuilder {
      */
     public ConversationBuilder blacklistWord(String word)
     {
+        List<Message> messages = (List<Message>)conversation.messages;
+        List<Message> newMessages = new ArrayList<>();
+        String regex = buildRegex(word);
+
+        for(Message msg:messages)
+        {
+            msg.content = msg.content.replaceAll(regex, redacted);
+        }
+
         return this;
         //TODO: Complete implementation of blacklistWord
+    }
+
+    /**
+     * Creates a regrex that finds individual words.
+     * The regex made is '\b[aA]word\b'
+     * where a is the first letter (lower case version) of the word
+     * A is the first letter (upper case version) of the word
+     * @param word The word to test for
+     * @return the regex in form mentioned above
+     */
+    private String buildRegex(String word)
+    {
+        StringBuilder sb = new StringBuilder(word);
+        String res;
+        char firstUpper = word.toUpperCase().toCharArray()[0];
+        char firstLower = word.toLowerCase().toCharArray()[0];
+        String set = "["+firstUpper +firstLower +"]";
+        sb.deleteCharAt(0);
+        res = "\\b"+ set + sb.toString() + "\\b";
+        return res;
+
     }
 }
--- src/test/java/com/mindlinksoft/recruitment/mychat/ConversationBuilderTests.java
+++ src/test/java/com/mindlinksoft/recruitment/mychat/ConversationBuilderTests.java
@@ -2,7 +2,11 @@ package com.mindlinksoft.recruitment.mychat;
 
 
 import org.junit.Test;
+
+import java.time.Instant;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ConversationBuilderTests {
 
@@ -10,21 +14,26 @@ public class ConversationBuilderTests {
      * Tests if the conversation has been correctly filtered according to the specified userId
      */
     @Test
-    public void testFilterByUser() throws Exception {
-        ConversationBuilder cb = prepareConversation();
-        String user;
+    public void testFilterByUser(){
+        try
+        {
+            ConversationBuilder cb = prepareConversation();
+            String user;
 
-        user = "bob";
-        cb.filterByUser(user);
-        Conversation c = cb.build();
+            user = "bob";
+            cb.filterByUser(user);
+            Conversation c = cb.build();
 
-        assertEquals(3, c.messages.size()); // should contain 3 messages
+            assertEquals(3, c.messages.size()); // should contain 3 messages
 
-        user = "CompleteStranger";
-        cb = prepareConversation();
-        cb.filterByUser(user);
-        c = cb.build();
-        assertEquals(0, c.messages.size()); // should contain 0 messages
+            user = "CompleteStranger";
+            cb = prepareConversation();
+            cb.filterByUser(user);
+            c = cb.build();
+            assertEquals(0, c.messages.size()); // should contain 0 messages
+        }catch(Exception e){
+            fail("chat.txt not found");
+        }
 
     }
 
@@ -32,22 +41,66 @@ public class ConversationBuilderTests {
      * Tests if the conversation has been correctly filtered according to the specified userId
      */
     @Test
-    public void testFilterByKeyword() throws Exception {
-        ConversationBuilder cb = prepareConversation();
-        String keyword;
+    public void testFilterByKeyword(){
+        try{
+            ConversationBuilder cb = prepareConversation();
+            String keyword;
 
-        keyword = "pie";
-        cb.filterByKeyword(keyword);
-        Conversation c = cb.build();
+            keyword = "pie";
+            cb.filterByKeyword(keyword);
+            Conversation c = cb.build();
 
-        assertEquals(4, c.messages.size()); // should contain 3 messages
+            assertEquals(4, c.messages.size()); // should contain 3 messages
 
-        keyword = "pasty";
-        cb = prepareConversation();
-        cb.filterByUser(keyword);
-        c = cb.build();
-        assertEquals(0, c.messages.size()); // should contain 0 messages
+            keyword = "pasty";
+            cb = prepareConversation();
+            cb.filterByUser(keyword);
+            c = cb.build();
+            assertEquals(0, c.messages.size()); // should contain 0 messages
+        }
+        catch (Exception e)
+        {
+            fail("unable to retrieve chat.txt");
+        }
 
+    }
+
+    /**
+     * Test helper method to build regex that is used find blacklisted words
+     */
+    @Test
+    public void testBlacklistWord()
+    {
+        try
+        {
+            String blacklistWord = "pie";
+            String blacklistWord2 = "no";
+            ConversationBuilder cb = prepareConversation();
+            cb.blacklistWord(blacklistWord);
+            cb.blacklistWord(blacklistWord2);
+            Conversation c = cb.build();
+            Message[] ms = new Message[c.messages.size()];
+            c.messages.toArray(ms);
+
+            assertEquals("Hello there!", ms[0].content);
+
+            assertEquals("how are you?", ms[1].content);
+
+            assertEquals("I'm good thanks, do you like *redacted*?", ms[2].content);
+
+            assertEquals("*redacted*, let me ask Angus...", ms[3].content);
+
+            assertEquals("Hell yes! Are we buying some *redacted*?", ms[4].content);
+
+            assertEquals("*redacted*, just want to know if there's anybody else in the *redacted* society...", ms[5].content);
+
+            assertEquals("YES! I'm the head *redacted* eater there...", ms[6].content);
+
+
+        } catch (Exception e)
+        {
+            fail("unable to retrieve chat.txt");
+        }
     }
 
 
