539c12a7837b2dd2011ef11509967d74b85faf4e
Julian Kyotaro Kubelec <juliankubelec23@gmail.com>
2021-03-04 21:55:19 +0000

    Removed I/O use from prepareConversation in ConversationBuilderTests

--- src/test/java/com/mindlinksoft/recruitment/juliankubelec/mychat/ConversationBuilderTests.java
+++ src/test/java/com/mindlinksoft/recruitment/juliankubelec/mychat/ConversationBuilderTests.java
@@ -3,6 +3,9 @@ package com.mindlinksoft.recruitment.juliankubelec.mychat;
 
 import org.junit.Test;
 
+import java.time.Instant;
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
@@ -115,14 +118,30 @@ public class ConversationBuilderTests {
 
 
     /**
-     * Helper function to initialise the default version of the conversation from chat.txt
-     * @throws Exception error if the exporter was unable to read the conversation (e.g. file doesn't exist)
+     * Helper function to initialise a mock of the conversation from chat.txt
      * @return the original conversation
      */
-    private ConversationBuilder prepareConversation() throws Exception
+    private ConversationBuilder prepareConversation()
     {
-        ConversationExporter exporter = new ConversationExporter();
-        Conversation conversation = exporter.readConversation("chat.txt");
+        String name = "My Conversation";
+        ArrayList<Message> messages = new ArrayList<>();
+                messages.add(new Message(Instant.ofEpochSecond(1448470901),
+                        "bob", "Hello there!"));
+                messages.add(new Message(Instant.ofEpochSecond(1448470905),
+                        "mike", "how are you?"));
+                messages.add(new Message(Instant.ofEpochSecond(1448470906),
+                        "bob", "I 'm good thanks, do you like pie?"));
+                messages.add(new Message(Instant.ofEpochSecond(1448470910),
+                        "mike", "no, let me ask Angus..."));
+                messages.add(new Message(Instant.ofEpochSecond(1448470912),
+                        "angus", "Hell yes !Are we buying some pie?"));
+                messages.add(new Message(Instant.ofEpochSecond(1448470914),
+                        "bob", "No, just want to know if there 's anybody else in the pie society..."));
+                messages.add(new Message(Instant.ofEpochSecond(1448470915),
+                        "angus", "YES !I 'm the head pie eater there..."));
+
+        Conversation conversation = new Conversation(name, messages);
+
         return new ConversationBuilder(conversation);
     }
 
